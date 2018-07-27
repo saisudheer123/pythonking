e5fe6e7239d20a6b5ba184085be947ab8db24f56
saisunil702 <41770017+saisunil702@users.noreply.github.com>
2018-07-27 16:10:23 +0530

    Create sunil3.py

---
+++ sunil3.py
@@ -0,0 +1,8 @@
+x=input()
+if(x>='a' and x<='z') or (x>='A' and x<='Z'):
+	if x in ['a','e','i','o','u','A','E','I','O','U']:
+		print("Vowel")
+	else:
+	            print("Consonant")
+else:
+	print("invalid")
