b4affb93d86fb444dca29abe7d1ba601f7798233
saisunil702 <41770017+saisunil702@users.noreply.github.com>
2018-07-27 14:52:37 +0530

    Update sunil2.py

--- sunil2.py
+++ sunil2.py
@@ -1,8 +1,8 @@
 a=int(raw_input())
 if(int(a)>=0):
-if(int(a%2)==0):
-print("Even")
-else:
-print("Odd")
-else:
-print("Invalid")
+  if(int(a%2)==0):
+    print("Even")
+    else:
+      print("Odd")
+ else:
+  print("Invalid")
