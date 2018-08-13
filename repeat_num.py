582675de3a3ee3be3d0e493f17111f381716b6f3
saisunil702 <41770017+saisunil702@users.noreply.github.com>
2018-08-13 12:04:29 +0530

    Update repeat_num.py

--- repeat_num.py
+++ repeat_num.py
@@ -1,17 +1,17 @@
-b=input()
-c=len(b)
+b=raw_input()
 q=0
 w=0
 new=[]
-list=[int(x) for x in input().split()]
+list=[int(x) for x in raw_input().split()]
+c=len(list)
 while(q==0):
     l=list[q]
     list.remove(list[0])
     if((l in list)and(l not in new)):
-        print(l,end=' ')
+        print l,
         new.insert(w,l)
         w+=1
-    elif((list==new)and(l not in new)):
+    elif((list==new)and(l not in new)and(c==len(new)):
         print('unique')
     else:
         q=0
