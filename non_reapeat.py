0f0ef9a8ef45813503523555773f810c53d5dc3f
saisunil702 <41770017+saisunil702@users.noreply.github.com>
2018-08-13 12:55:39 +0530

    Create non_reapeat.py

---
+++ non_reapeat.py
@@ -0,0 +1,18 @@
+b=raw_input()
+c=len(b)
+q=0
+w=0
+new=[]
+list=[int(x) for x in raw_input().split()]
+while(q==0):
+    l=list[q]
+    list.remove(list[0])
+    if((l in list)and(l not in new)):
+        new.insert(w,l)
+        w+=1
+    elif(l not in new):
+        print l,
+    else:
+        q=0
+    if(list==[]):
+        break
