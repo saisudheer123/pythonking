ae13dab16b68b08f61d6b578cb65b44b2de6e296
saisunil702 <41770017+saisunil702@users.noreply.github.com>
2018-08-13 11:52:30 +0530

    Create repeat_num.py

---
+++ repeat_num.py
@@ -0,0 +1,19 @@
+b=input()
+c=len(b)
+q=0
+w=0
+new=[]
+list=[int(x) for x in input().split()]
+while(q==0):
+    l=list[q]
+    list.remove(list[0])
+    if((l in list)and(l not in new)):
+        print(l,end=' ')
+        new.insert(w,l)
+        w+=1
+    elif((list==new)and(l not in new)):
+        print('unique')
+    else:
+        q=0
+    if(list==[]):
+        break
