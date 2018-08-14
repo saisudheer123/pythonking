b6499360d375f9a50ab115f75cb923d03d17a76c
saisunil702 <41770017+saisunil702@users.noreply.github.com>
2018-08-14 14:05:16 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,9 +1,9 @@
 nom={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-b=[str(x)for x in input()]
+bom=[str(q)for q in input()]
 s=0
-for i in range(0,len(b)):
-    if i>0 and nom[b[i]] > nom[b[i-1]]:
-        s=nom[b[i]]-nom[b[i-1]]
+for i in range(0,len(bom)):
+    if i>0 and nom[bom[i]] > nom[bom[i-1]]:
+        s=nom[bom[i]]-nom[bom[i-1]]
     else:
-        s+=nom[b[i]]
+        s+=nom[bom[i]]
 print(s)
